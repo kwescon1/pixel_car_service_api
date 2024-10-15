403e57e858d9d05638965fb4e0d41b9017c533a0
Kwesi Danquah <kwescon1@gmail.com>
2024-10-16 00:52:16 +0100

    update exception handling with custom responses for not found, authentication, and general errors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,18 @@
 <?php
 
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Foundation\Application;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -27,9 +27,9 @@ return Application::configure(basePath: dirname(__DIR__))
             return response()->error($e, Response::HTTP_UNAUTHORIZED);
         });
 
-
         $exceptions->renderable(function (Exception $e) {
-            Log::error($e->getMessage() . "\n" . $e->getTraceAsString());
+            Log::error($e->getMessage()."\n".$e->getTraceAsString());
+
             return response()->error($e);
         });
     })->create();
