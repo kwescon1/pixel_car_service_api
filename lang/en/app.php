1959de1135f5f0e206decdfc60aa199af31351df
Kwesi Danquah <kwescon1@gmail.com>
2024-10-15 23:42:36 +0100

    add localization language lines for common application messages

---
+++ lang/en/app.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    // feat: Add localization language lines for common application messages
+    /*
+    |--------------------------------------------------------------------------
+    | Application Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in application for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    // Application general messages
+    'operation_successful' => 'Operation successful!',
+    'resource_created' => 'Resource created!',
+    'resource_not_found' => 'Resource not found!',
+    'validation_failed' => 'Validation failed!',
+
+    // Add other messages if needed
+];
