e35b94cd23241654b8f392b462f89d6a2231d49b
Kwesi Danquah <kwescon1@gmail.com>
2024-10-16 00:54:35 +0100

    make AppServiceProvider deferrable and register routeNames as singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,21 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+class AppServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     /**
      * Register any application services.
      */
     public function register(): void
     {
-        //
+
+        // Register route names as a singleton, making it globally accessible
+        $this->app->singleton('routeNames', function () {
+            return config('routes');
+        });
     }
 
     /**
