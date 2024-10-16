3f33d8fe78b031ca2ea9a6fae888b8ad0f5c5688
Kwesi Danquah <kwescon1@gmail.com>
2024-10-16 17:04:58 +0100

    Implemented Car service crud

---
+++ app/Services/CarService/CarServiceService.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Services\CarService;
+
+
+use App\Models\CarService;
+use App\Models\ServiceType;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+use App\Interfaces\CarService\CarServiceInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class CarServiceService implements CarServiceInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct() {}
+
+    public function storeCarService(array $data, ServiceType $serviceType): CarService
+    {
+
+        return $serviceType->carServices()->create($data);
+    }
+
+    public function getCarServices(): LengthAwarePaginator
+    {
+        return CarService::with('serviceType')->paginate(10);
+    }
+
+    public function getCarService(string $id): CarService
+    {
+        $carService = CarService::with('serviceType')->whereUuid($id)->first();
+
+        if (!$carService) {
+            throw new NotFoundHttpException(__('app.resource_not_found'));
+        }
+
+        return $carService;
+    }
+
+    public function deleteCarService(string $id): bool
+    {
+        $carService = $this->getCarService($id);
+
+        if (!$carService) {
+            throw new NotFoundHttpException(__('app.resource_not_found'));
+        }
+
+        return $carService->delete();
+    }
+    public function updateCarService(array $data, string $id): CarService
+    {
+
+        $carService = $this->getCarService($id);
+
+        // Update the car service with the new data
+        $carService->update($data);
+
+        // Return the updated car service
+        return $carService;
+    }
+}
