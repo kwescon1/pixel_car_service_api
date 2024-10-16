5ae56158bfa9ccb050b4efb2a0ea01b8edb47fc2
Kwesi Danquah <kwescon1@gmail.com>
2024-10-16 12:21:38 +0100

    updated admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -16,11 +15,17 @@ class AdminSeeder extends Seeder
     {
         // Retrieve the admin password from the .env file
         $adminPassword = env('ADMIN_PASSWORD', 'default_password');
-        // Create a new admin user
-        User::firstOrCreate(['email' => 'admin@pixel.com'], [
-            'name' => 'Pixel Admin',
-            'password' => Hash::make($adminPassword), // Secure password
-            'email_verified_at' => Carbon::now(), // Secure password
-        ]);
+        $adminEmail = env('ADMIN_EMAIL', 'default_email');
+
+        // Create a new admin user, password will be automatically hashed
+        User::firstOrCreate(
+            ['email' => $adminEmail],
+            [
+                'name' => 'Pixel Admin',
+                'password' => $adminPassword,
+                'email_verified_at' => Carbon::now(),
+                'is_admin' => true,
+            ]
+        );
     }
 }
