12021b892f9e60d15794b0bf9a4b84254a7b329a
Kwesi Danquah <kwescon1@gmail.com>
2024-10-16 09:10:10 +0100

    Registered authorization exception in handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -10,9 +11,9 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -27,8 +28,12 @@ return Application::configure(basePath: dirname(__DIR__))
             return response()->error($e, Response::HTTP_UNAUTHORIZED);
         });
 
+        $exceptions->renderable(function (AuthorizationException $e) {
+            return response()->error($e, Response::HTTP_FORBIDDEN);
+        });
+
         $exceptions->renderable(function (Exception $e) {
-            Log::error($e->getMessage()."\n".$e->getTraceAsString());
+            Log::error($e->getMessage() . "\n" . $e->getTraceAsString());
 
             return response()->error($e);
         });
