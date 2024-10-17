91571431f7d0fb39c84f2a60a722817375889517
Kwesi Danquah <kwescon1@gmail.com>
2024-10-17 17:54:32 +0100

    implemented ction for uploading images

---
+++ app/Actions/UploadImageAction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Actions;
+
+use App\Jobs\ProcessImage;
+use App\Models\Mechanic;
+use Illuminate\Http\UploadedFile;
+
+class UploadImageAction
+{
+    /**
+     * Execute the action to handle image upload.
+     *
+     * @param Mechanic $mechanic
+     * @param UploadedFile|null $image
+     */
+    public function execute(Mechanic $mechanic, ?UploadedFile $image): void
+    {
+        // Dispatch the job to process the image upload
+        if ($image) {
+            // Store the image temporarily in the 'mechanics' folder
+            $filePath = $image->store('mechanics/tmp', 'public');
+
+            ProcessImage::dispatch($mechanic, $filePath);
+        }
+    }
+}
