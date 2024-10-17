072b06d17841654a375162a3cf3c1c7d5716eb1a
Kwesi Danquah <kwescon1@gmail.com>
2024-10-17 23:03:40 +0100

    Registered application contracts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,16 @@
 namespace App\Providers;
 
 use App\Interfaces\Auth\AuthServiceInterface;
+use App\Interfaces\BookingDate\BookingDateServiceInterface;
 use App\Interfaces\CarService\CarServiceInterface;
+use App\Interfaces\Mechanic\MechanicServiceInterface;
 use Illuminate\Support\ServiceProvider;
 use App\Services\ServiceType\ServiceTypeService;
 use App\Interfaces\ServiceType\ServiceTypeInterface;
 use App\Services\Auth\AuthService;
+use App\Services\BookingDate\BookingDateService;
 use App\Services\CarService\CarServiceService;
+use App\Services\Mechanic\MechanicService;
 use Illuminate\Contracts\Support\DeferrableProvider;
 
 class AppServiceProvider extends ServiceProvider implements DeferrableProvider
@@ -23,6 +27,8 @@ class AppServiceProvider extends ServiceProvider implements DeferrableProvider
         $this->app->bind(ServiceTypeInterface::class, ServiceTypeService::class);
         $this->app->bind(AuthServiceInterface::class, AuthService::class);
         $this->app->bind(CarServiceInterface::class, CarServiceService::class);
+        $this->app->bind(MechanicServiceInterface::class, MechanicService::class);
+        $this->app->bind(BookingDateServiceInterface::class, BookingDateService::class);
     }
 
 
@@ -41,6 +47,6 @@ class AppServiceProvider extends ServiceProvider implements DeferrableProvider
      */
     public function provides(): array
     {
-        return [ServiceTypeInterface::class, AuthServiceInterface::class, CarServiceInterface::class];
+        return [ServiceTypeInterface::class, AuthServiceInterface::class, CarServiceInterface::class, MechanicServiceInterface::class, BookingDateServiceInterface::class];
     }
 }
