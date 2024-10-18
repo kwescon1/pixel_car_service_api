ff8698c6a7a389b4a471ff17615fb83fa697daf9
Kwesi Danquah <kwescon1@gmail.com>
2024-10-18 17:33:46 +0100

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,7 @@ class DatabaseSeeder extends Seeder
 
         //seed the car services
         $this->call(CarServiceSeeder::class);
+
+        $this->call(MechanicAvailabilitySeeder::class);
     }
 }
